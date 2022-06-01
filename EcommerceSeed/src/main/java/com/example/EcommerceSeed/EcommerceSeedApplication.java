cb9b88ec27e058c82ad375497880f4b8e8b5532e
yonseok <dmzld94@gmail.com>
2022-06-01 14:22:48 +0900

    Initial commit

---
+++ EcommerceSeed/src/main/java/com/example/EcommerceSeed/EcommerceSeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceSeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSeedApplication.class, args);
+	}
+
+}
---
+++ EcommerceSeed/src/test/java/com/example/EcommerceSeed/EcommerceSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceSeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
